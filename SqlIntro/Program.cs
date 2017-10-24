8968d6e1baafde2990e92b25d7e1beab34ae0a4e
Zach Phillips <zjohnny94@gmail.com>
2017-10-24 14:57:04 -0500

    Pointed repo to SimpleDB from ProductRepository

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -8,7 +8,7 @@ namespace SqlIntro
         static void Main(string[] args)
         {
             var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=unsecuresqlpass;"; //get connectionString format from connectionstrings.com and change to match your database
-            var repo = new ProductRepository(connectionString);
+            var repo = new SimpleDB(connectionString);
             foreach (var prod in repo.GetProducts())
             {
                 Console.WriteLine("Product Name:" + prod.Name + " " +
