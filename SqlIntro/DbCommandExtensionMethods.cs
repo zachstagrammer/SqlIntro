46ea68c08c6f1b231385718c7e90c45adf01a2f9
Zach Phillips <zjohnny94@gmail.com>
2017-10-23 22:24:17 -0500

    Added DbCommandExtensionMethods class

---
+++ SqlIntro/DbCommandExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace SqlIntro
+{
+    public static class DbCommandExtensionMethods
+    {
+        public static void AddParam(this IDbCommand command, string name, object value)
+        {
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+    }
+}
